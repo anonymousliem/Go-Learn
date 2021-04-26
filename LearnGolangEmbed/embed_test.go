f9ff80d766a334137d06fea3faed991dc94f6ae9
anonymousliem <muslimsyuhadah@gmail.com>
2021-04-26 13:23:01 +0700

    learn golang embed

---
+++ LearnGolangEmbed/embed_test.go
@@ -0,0 +1,14 @@
+package LearnGolangEmbed
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestEmbedString(t *testing.T) {
+	fmt.Println(version)
+}
