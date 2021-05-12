a131e437157c8fc54f2e458f10b2b55271bf33f0
anonymousliem <muslimsyuhadah@gmail.com>
2021-05-12 11:44:16 +0700

    Fizz Buzz Code Interview

---
+++ coding-interview-golang/fizzbuzz_test.go
@@ -0,0 +1,24 @@
+package coding_interview_golang
+
+import (
+	"fmt"
+	"testing"
+)
+
+func FizzBuzz(number int)  {
+	for i := 1; i < number; i++ {
+		if(i % 3 == 0 && i % 5 ==0){
+			fmt.Println("FizzBuzz")
+		}else if(i % 3 == 0) {
+			fmt.Println("Fizz")
+		}else if (i % 5 == 0) {
+			fmt.Println("buzz")
+		}else{
+			fmt.Println(i)
+		}
+	}
+}
+
+func TestFizzbuzz(t *testing.T) {
+	FizzBuzz(100)
+}
