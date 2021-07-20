d97e7477ddb2526b75d06e4ead557c798617441b
anonymousliem <muslimsyuhadah@gmail.com>
2021-07-20 22:48:28 +0700

    variadic function

---
+++ Go-Learn-Unit-Test/variadic.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(nums ...int) int {
+	s := 0
+	for _, i := range nums {
+		s += i
+	}
+	return s
+}
+
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println(sum(numbers...))
+}
